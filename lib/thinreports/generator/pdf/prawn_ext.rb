788633b233c40bdb2555e4720faacc462ebb6073
Katsuya HIDAKA <hidakatsuya@gmail.com>
2016-02-27 11:32:12 +0000

    Disable monkey-patching for `Prawn::Document#width_of`

--- lib/thinreports/generator/pdf/prawn_ext.rb
+++ lib/thinreports/generator/pdf/prawn_ext.rb
@@ -1,15 +1,6 @@
 # coding: utf-8
 
 module Prawn
-  class Document
-    alias_method :original_width_of, :width_of
-
-    def width_of(string, options={})
-      font.compute_width_of(string, options) +
-        (character_spacing * (font.character_count(string) - 1))
-    end
-  end
-
   module Images
     class Image
       alias_method :original_calc_image_dimensions, :calc_image_dimensions
